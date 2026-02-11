a018379f6e97863eff6a8c3895bc4225b4e26c23
Dnavas-28 <darwinjosenavas@gmail.com>
2026-02-10 20:03:58 -0500

    Updated HomePage.tsx

--- app/_components/HomePage.tsx
+++ app/_components/HomePage.tsx
@@ -54,7 +54,7 @@ export default function HomePage() {
         cta={t.services.software.cta}
         coreCapabilitiesLabel={t.labels.coreCapabilities}
         gradient="bg-gradient-to-br from-blue-500 to-blue-700"
-        backgroundColor="bg-white dark:bg-slate-800"
+        backgroundColor="bg-blue-50 dark:bg-slate-800"
         iconPosition="right"
       />
 
@@ -80,7 +80,7 @@ export default function HomePage() {
         cta={t.services.network.cta}
         coreCapabilitiesLabel={t.labels.coreCapabilities}
         gradient="bg-gradient-to-br from-cyan-500 to-cyan-700"
-        backgroundColor="bg-white dark:bg-slate-800"
+        backgroundColor="bg-cyan-50 dark:bg-slate-800"
         iconPosition="right"
       />
 
@@ -93,7 +93,7 @@ export default function HomePage() {
         cta={t.services.iso.cta}
         coreCapabilitiesLabel={t.labels.coreCapabilities}
         gradient="bg-gradient-to-br from-purple-500 to-purple-700"
-        backgroundColor="bg-white dark:bg-slate-800"
+        backgroundColor="bg-purple-50 dark:bg-slate-800"
         iconPosition="left"
       />
 
@@ -106,7 +106,7 @@ export default function HomePage() {
         cta={t.services.backups.cta}
         coreCapabilitiesLabel={t.labels.coreCapabilities}
         gradient="bg-gradient-to-br from-orange-500 to-orange-700"
-        backgroundColor="bg-white dark:bg-slate-800"
+        backgroundColor="bg-orange-50 dark:bg-slate-800"
         iconPosition="right"
       />
 
@@ -119,7 +119,7 @@ export default function HomePage() {
         cta={t.services.licensing.cta}
         coreCapabilitiesLabel={t.labels.coreCapabilities}
         gradient="bg-gradient-to-br from-indigo-500 to-indigo-700"
-        backgroundColor="bg-white dark:bg-slate-800"
+        backgroundColor="bg-indigo-50 dark:bg-slate-800"
         iconPosition="left"
       />
 
@@ -132,7 +132,7 @@ export default function HomePage() {
         cta={t.services.disasterRecovery.cta}
         coreCapabilitiesLabel={t.labels.coreCapabilities}
         gradient="bg-gradient-to-br from-rose-500 to-rose-700"
-        backgroundColor="bg-white dark:bg-slate-800"
+        backgroundColor="bg-rose-50 dark:bg-slate-800"
         iconPosition="right"
       />
 
@@ -145,7 +145,7 @@ export default function HomePage() {
         cta={t.services.dataCenter.cta}
         coreCapabilitiesLabel={t.labels.coreCapabilities}
         gradient="bg-gradient-to-br from-teal-500 to-teal-700"
-        backgroundColor="bg-white dark:bg-slate-800"
+        backgroundColor="bg-teal-50 dark:bg-slate-800"
         iconPosition="left"
         checkColor="text-teal-500"
       />
